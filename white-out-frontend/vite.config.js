59e3750caf36ea84fc99ab8e11e41d197f5c2812
jbrennan414 <brennanj414@gmail.com>
2024-11-11 16:35:26 -0700

    update vite config

--- white-out-frontend/vite.config.js
+++ white-out-frontend/vite.config.js
@@ -1,6 +1,6 @@
-// vite.config.js
-export default {
-  build: {
-    outDir: 'dist'  // Default output directory
-  }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()]
+})
