779236cbdea7c56309b3f0b2e1f0d4f8335455bb
jbrennan414 <brennanj414@gmail.com>
2024-11-10 20:10:04 -0700

    Initial commit

---
+++ test/white-out-ski.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const WhiteOutSki = require('../lib/white-out-ski-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/white-out-ski-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new WhiteOutSki.WhiteOutSkiStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
